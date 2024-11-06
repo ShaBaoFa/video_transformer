09c271b754fb4ee618ab47d46972ee918fadb25c
wlfpanda1012 <wlfpanda1012@gmail.com>
2024-11-06 13:04:49 +0800

    支持 publishes

--- src/TransformerProvider.php
+++ src/TransformerProvider.php
@@ -11,6 +11,16 @@ class TransformerProvider extends ServiceProvider
         $this->registerConfig();
     }
 
+    public function boot()
+    {
+        $configPath = __DIR__ . '/publish/transformer.php';
+        if (function_exists('config_path')) {
+            $publishPath = config_path('transformer.php');
+        } else {
+            $publishPath = base_path('config/transformer.php');
+        }
+        $this->publishes([$configPath => $publishPath], 'transformer-config');
+    }
     protected function registerConfig(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/publish/transformer.php', 'transformer');
